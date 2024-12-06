532369c86b0b849ee0a0be89ea1c993fa23b107d
My name <you44@example.com>
2024-12-06 18:47:45 +0200

    Add validation for SpyCat model on field breed

--- cat/serializers.py
+++ cat/serializers.py
@@ -1,3 +1,4 @@
+import requests
 from rest_framework import serializers
 from .models import SpyCat
 
@@ -10,4 +11,14 @@ class SpyCatSerializer(serializers.ModelSerializer):
     def validate_experience(self, value):
         if value < 0:
             raise serializers.ValidationError("Experience cannot be negative.")
+        return value
+
+    def validate_breed(self, value):
+        response = requests.get("https://api.thecatapi.com/v1/breeds/")
+        if response.status_code != 200:
+            raise serializers.ValidationError("Unable to fetch breed data at the moment.")
+
+        breeds = [breed['name'] for breed in response.json()]
+        if value not in breeds:
+            raise serializers.ValidationError(f"'{value}' is not a valid breed. Please provide a valid breed.")
         return value
